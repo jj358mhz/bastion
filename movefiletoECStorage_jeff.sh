a3a27e413f4ce25cfd948460c1373423a1f8018e
Jeff Johnston <jj358mhz@gmail.com>
2023-07-27 12:11:49 -0700

    Working script

--- movefiletoECStorage_jeff.sh
+++ movefiletoECStorage_jeff.sh
@@ -4,21 +4,25 @@
 CDNUser="jjohnston@edg.io@rsync.vny.EEA8.labcdn.com:"
 CDNDownloadURL="http://cdn.telcomjj.com"
 
-# Get user identity
-User=$(whoami | awk -F '\\' '{print $2}')
-echo "$User"
+# Get user identity and remove unwanted characters from the username
+User=$(whoami | awk -F '\\' '{print $2}' | tr -d '\r')
+echo "The user is: $User"
+echo ""
 
 # Get ssh key of the user
-UKey=$(find "/Users/$User/.ssh/" -printf "%f\n" | egrep "^id_.*[[:digit:]]$")
-echo "$UKey"
+UKey=$(ls "/home/ecdc/$User/.ssh"/id_* 2>/dev/null | head -n 1)
+echo "The user ssh key is: $UKey"
+echo ""
 
 # Get Random FTP server in prod
 FTPServer=$(bselect ftp grq | sort --random-sort | tail -1)
-echo "$FTPServer"
+echo "The random FTP server to be used is: $FTPServer"
+echo ""
+
 echo "Make sure your key has already been signed!"
-echo "Do you want to continue?"
-read -p "Are you sure? [y/n]: " -n 1 -r
+read -p "Do you want to continue? [y/n]: " -n 1 -r
 echo    # (optional) move to a new line
+
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "yes"
     # Get the server name and check dns before proceeding.
@@ -34,6 +38,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     fi
     # Set the path to place file on remote host.
     read -p "Enter the remote cdn endpoint path where the file is to be placed and press [ENTER] (i.e. /fox/for_download/for_pavel): " RemoteServerPath
+    echo ""
     echo "$RemoteServerPath"
     if [ -z "$RemoteServerPath" ]; then
         echo "Whoops!, you did not enter anything, exiting"
@@ -42,6 +47,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
     # Get the file structure and make sure it is not blank
     echo "Example of the path would be /var/Edgecast/logs/uplynk/uplynk_liveslicer-kcpq_fxb_p.log.tar.bz2"
+    echo ""
     read -p "Enter the full path of the location of the file you would like to move and press [ENTER]: " PathandFile
     echo "$PathandFile"
     if [ -z "$PathandFile" ]; then
@@ -72,14 +78,18 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         ActualFile=$(echo "$PathandFile" | awk -F "/" '{print $NF}')
         changedActualFile="$(date +"%H-%M_%d-%m-%Y")$ActualFile"
-        /usr/bin/scp -4 -l "$Speed" -i "/home/ecdc/$User/.ssh/$UKey" "root@$ServerName:$PathandFile" "/home/ecdc/$User/$changedActualFile"
+        /usr/bin/scp -4 -l "$Speed" -i "$UKey" "root@$ServerName:$PathandFile" "/home/ecdc/$User/$changedActualFile"
         echo "File Complete"
         echo "Moving file to Storage"
         # /usr/bin/scp -4 -i /home/ecdc/$User/.ssh/$UKey /home/ecdc/$User/$changedActualFile root@$FTPServer:/EC_Storage/grq001/1176C/appsupport/
-        /usr/bin/scp -4 -i "/home/ecdc/$User/.ssh/$UKey" "/home/ecdc/$User/$changedActualFile" "$CDNUser$RemoteServerPath"
+        /usr/bin/scp -4 -i "$UKey" "/home/ecdc/$User/$changedActualFile" "$CDNUser$RemoteServerPath"
+        echo ""
         echo "The file has been moved"
+        echo ""
         echo "Please test with a curl:"
+        echo ""
         echo "curl -O $CDNDownloadURL$RemoteServerPath/$changedActualFile"
+        echo ""
         read -p "Would you like to delete the file from bast to keep things clean? [y/n] " -n 1 -r
         if [[ $REPLY =~ ^[Yy]$ ]]; then
             rm "/home/ecdc/$User/$changedActualFile"
